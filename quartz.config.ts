f8f95592aea7cb62148ec74b9f7061d71f5623af
stefunspace <stefunspace@gmail.com>
2026-01-20 22:34:31 -0800

    Quartz sync: Jan 20, 2026, 10:34 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "https://stefunspace.github.io/anarres/",
+    baseUrl: "stefunspace.github.io/anarres/",
     ignorePatterns: ["admin", "private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
