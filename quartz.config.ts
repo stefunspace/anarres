0cf5f4c216556d8336cff543173861d491495a26
stefunspace <stefunspace@gmail.com>
2026-01-30 22:37:25 -0800

    Quartz sync: Jan 30, 2026, 10:37 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -23,8 +23,8 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Lora",
-        body: "Lora",
+        header: "Onest",
+        body: "Onest",
         code: "IBM Plex Mono",
       },
       colors: {
